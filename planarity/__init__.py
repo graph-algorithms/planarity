2580679ab44390225e2f985872eeeeb2d8f5496e
Wanda Boyer <wbkboyer@gmail.com>
2026-02-25 09:57:03 -0800

    Initial work to use pyproject.toml for setuptools configuration.
    We still need setup.py to build the extensions, since specifying ext_modules is still experimental and is only currently meant to support simple extensions; see https://github.com/pypa/setuptools/issues/5096
        - Also see https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html and https://setuptools.pypa.io/en/latest/userguide/ext_modules.html which only give example extensions that wrap a single C sourcefile

--- planarity/__init__.py
+++ planarity/__init__.py
@@ -1,5 +1,7 @@
-from __future__ import absolute_import
 from .planarity import PGraph
 from .planarity_functions import *
 from .planarity_networkx import *
-#from .tests.test import run as test
+
+# NOTE: In the future, we could automatically generate the version number by
+# configuring setuptools-scm, but presently this seems simpler.
+__version__ = "0.7.0"
--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 import os
 import sys
 
-import setuptools
-from setuptools import setup
-
-from distutils.extension import Extension
 from glob import glob
+from setuptools import setup, Extension
+
 
 try:
     from Cython.Build import cythonize
@@ -13,31 +11,6 @@ except ImportError:
     USE_CYTHON = False
 else:
     USE_CYTHON = True
-
-long_description  = """Graph planarity tools.
-A wrapper to Boyer's (C) planarity algorithms: http://code.google.com/p/planarity/
-
-Provides planarity testing, forbidden subgraph finding, and planar embeddings.
-
-Works with the NetworkX graph package (not required).
-"""
-
-classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Physics']
 
 ext = '.pyx' if USE_CYTHON else '.c'
 
@@ -54,21 +27,6 @@ if USE_CYTHON:
     extensions = cythonize(extensions)
 
 setup(
-    name= 'planarity',
-    packages=setuptools.find_packages(),
-    maintainer = 'Aric Hagberg',
-    maintainer_email = 'aric.hagberg@gmail.com',
-    author = 'Aric Hagberg',
-    author_email = 'aric.hagberg@gmail.com',
-    description = 'Graph planarity tools.',
-    classifiers = classifiers,
-    long_description = long_description,
-    license = 'BSD',
     ext_modules = extensions,
-    version          = '0.6',
-    url = 'https://github.com/hagberg/planarity/',
-    download_url='https://pypi.python.org/pypi/planarity',
-    package_data = {'planarity':['tests/*.py']},
-    zip_safe = False
     )
 
