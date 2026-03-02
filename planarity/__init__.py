0775d0ca930f1f836012537d512d382f6e038d2d
john-boyer-phd <john.boyer.phd@gmail.com>
2026-03-01 18:02:25 -0800

    Need to go to 1.0.0, but first need one or two more interim TestPyPI releases

--- planarity/__init__.py
+++ planarity/__init__.py
@@ -4,4 +4,4 @@ from .planarity_networkx import *
 
 # NOTE: In the future, we could automatically generate the version number by
 # configuring setuptools-scm, but presently this seems simpler.
-__version__ = "1.0.0"
+__version__ = "0.7.6"
