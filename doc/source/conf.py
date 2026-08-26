3945ef2166a8f9f3280dffda48037906175d1eed
Wanda B.K. Boyer <wbkboyer@gmail.com>
2026-08-25 17:59:05 -0700

    Fixed import error problem so that sphinx now consumes .so files in virtual environment site-packages rather than having to separately build_ext.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,9 +10,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # Problems with imports? Could try `export PYTHONPATH=$PYTHONPATH:`pwd`` from root project dir...
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('<path_to_other_module>'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
